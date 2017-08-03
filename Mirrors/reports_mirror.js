bd82b08c33d90435d0fd5494cf46b0648e814e6a
mrdmantoo <mrdmantoo@gmail.com>
2017-08-03 11:39:03 -0400

    Rename reports_mirror.js to Mirrors/reports_mirror.js

---
+++ Mirrors/reports_mirror.js
@@ -0,0 +1,1 @@
+alert ('An attempt made by your device to report back to Intelligo has been blocked.');
--- reports_mirror.js
+++
@@ -1,1 +0,0 @@
-alert ('An attempt made by your device to report back to Intelligo has been blocked.');
